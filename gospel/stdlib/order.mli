23f201bd083309ae26be1da310c4d642be0564aa
Cláudio Belo Lourenço <belolourenco@gmail.com>
2019-11-21 16:07:41 +0100

    Use integer_of_int coercion in order.mli

--- gospel/stdlib/order.mli
+++ gospel/stdlib/order.mli
@@ -9,15 +9,10 @@
 (**************************************************************************)
 
 (*@ predicate is_pre_order (cmp: 'a -> 'a -> int) =
-    (forall x. integer_of_int (cmp x x) = 0) /\
-    (forall x y. integer_of_int (cmp x y) <= 0 <->
-                   integer_of_int (cmp y x) >= 0) /\
+    (forall x. cmp x x = 0) /\
+    (forall x y. cmp x y <= 0 <-> cmp y x >= 0) /\
     (forall x y z.
-       (integer_of_int (cmp x y) <= 0 ->
-         integer_of_int (cmp y z) <= 0 -> integer_of_int (cmp x z) <= 0) /\
-       (integer_of_int (cmp x y) <= 0 ->
-         integer_of_int (cmp y z) <  0 -> integer_of_int (cmp x z) <  0) /\
-       (integer_of_int (cmp x y) <  0 ->
-         integer_of_int (cmp y z) <= 0 -> integer_of_int (cmp x z) <  0) /\
-       (integer_of_int (cmp x y) <  0 ->
-         integer_of_int (cmp y z) <  0 -> integer_of_int (cmp x z) <  0)) *)
+       (cmp x y <= 0 -> cmp y z <= 0 -> cmp x z <= 0) /\
+       (cmp x y <= 0 -> cmp y z <  0 -> cmp x z <  0) /\
+       (cmp x y <  0 -> cmp y z <= 0 -> cmp x z <  0) /\
+       (cmp x y <  0 -> cmp y z <  0 -> cmp x z <  0)) *)
