727f8569dc9929ab8708dcff63e947dab30f8550
Jean-Christophe Filliatre <Jean-Christophe.Filliatre@lri.fr>
2019-11-22 15:50:50 +0100

    fixed custom rule for gospel/ocaml-lib/gospelstdlib.ml

--- gospel/misc/file_to_string.ml
+++ gospel/misc/file_to_string.ml
@@ -2,7 +2,14 @@
 open Format
 
 let input = Sys.argv.(1)
+let output = Sys.argv.(2)
+
 let ic = open_in_bin input
 let contents = really_input_string ic (in_channel_length ic)
 let () = close_in ic
-let () = printf "let contents = %S@." contents
+
+let oc = open_out output
+let fmt = formatter_of_out_channel oc
+let () = fprintf fmt "let contents = %S@." contents
+let () = close_out oc
+
