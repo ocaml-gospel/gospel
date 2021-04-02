fa2529930352156e5deab7652df09d9a955d046b
Mário Pereira <mjp.pereira@fct.unl.pt>
2021-04-02 18:04:55 +0100

    Fixed location and filename for OCaml structures and GOSPEL spec.

--- src/parser_frontend.ml
+++ src/parser_frontend.ml
@@ -59,7 +59,7 @@ let parse_ocaml_structure_lb lb =
 let parse_ocaml_structure_lb lb =
   (* TODO: extend pre-processor to handle structures *)
   (* let lb_pps = Pps.run lb |> Lexing.from_string in *)
-  Location.init lb lb.lex_start_p.pos_fname;
+  Location.init lb lb.lex_curr_p.pos_fname;
   try Parser.implementation Lexer.token lb with
     Parser.Error ->
     let loc_start, loc_end = lb.lex_start_p, lb.lex_curr_p in
