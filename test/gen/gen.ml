3bbbc31fd77ce60278ed164222641ead63797324
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-07-05 13:34:54 +0200

    Improve dependencies in generated dune.inc files
    Teach gen.exe to use the dependencies given on the commandline rather
    than a too generic `(source_tree .)`
    On the way, clean up the generated dune.inc files by using more implicit
    dependencies

--- test/gen/gen.ml
+++ test/gen/gen.ml
@@ -1,19 +1,43 @@
+(* Generates the dune configuration for the current directory of tests
+
+   Takes dependencies as arguments, to be read as a sequence of pairs,
+   such that:
+     gen.exe a.mli b.mli c.mli "d.mli e.mli"
+   will generate the configuration with a.mli depending on b.mli and
+   c.mli depending on both d.mli and e.mli. *)
+
+let dependencies =
+  let n = Array.length Sys.argv in
+  assert (n mod 2 = 1);
+  let n = n / 2 in
+  let tbl = Hashtbl.create n in
+  for i = 1 to n do
+    Hashtbl.add tbl Sys.argv.((i * 2) - 1) Sys.argv.(i * 2)
+  done;
+  tbl
+
 let print_rule file =
   if Filename.extension file = ".mli" then
+    let deps =
+      match Hashtbl.find_all dependencies file with
+      | [] -> ""
+      | ds -> "\n  " ^ String.concat "\n  " ds
+    in
     Printf.printf
       {|(rule
- (target %s.output)
- (deps (source_tree .))
+ (deps
+  (:checker %%{project_root}/test/gospel_check.exe)%s)
  (action
-   (with-outputs-to %%{target}
-      (run %%{project_root}/test/gospel_check.exe %%{dep:%s}))))
+  (with-outputs-to %s.output
+   (run %%{checker} %%{dep:%s}))))
 
 (rule
  (alias runtest)
- (action (diff %%{dep:%s} %%{dep:%s.output})))
+ (action
+  (diff %s %s.output)))
 
 |}
-      file file file file
+      deps file file file file
 
 let () =
   let files = Filename.current_dir_name |> Sys.readdir in
