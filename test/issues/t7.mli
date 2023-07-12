8fce1c27818f1d40a3f95e817e8e35be6ed7310e
Nicolas Osborne <nicolas.osborne@tarides.com>
2023-07-12 16:35:19 +0200

    fix Dterm.dpattern to Tterm.pattern with cast
    As translation from Dterm.dpattern to Tterm.pattern is done after
    typing, type informations are not needed anymore.

--- test/issues/t7.mli
+++ test/issues/t7.mli
@@ -5,7 +5,5 @@ val f : int option -> int
              | None -> true *)
 
 (* {gospel_expected|
-   [125] gospel: internal error, uncaught exception:
-                 File "src/dterm.ml", line 332, characters 18-24: Assertion failed
-                 
+   [0] OK
    |gospel_expected} *)
