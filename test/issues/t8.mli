23c8fe90bed485aed0e3ccf2e6c90cb70b4199f9
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-07-13 11:33:50 +0200

    Add two issues

---
+++ test/issues/t8.mli
@@ -0,0 +1,9 @@
+(* Probably the same root issue than t7 *)
+
+open Stdlib.Float.Array
+
+(* {gospel_expected|
+   [125] gospel: internal error, uncaught exception:
+                 Not_found
+                 
+   |gospel_expected} *)
