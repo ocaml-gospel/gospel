a61f5d5fca1a9f61ddc59c512821d6b7377ddaeb
Samuel Hym <samuel.hym@rustyne.lautre.net>
2023-11-06 18:01:35 +0100

    Remove the `gospel_expected` prologue at the end of successful tests

--- test/utils/testchecker.ml
+++ test/utils/testchecker.ml
@@ -21,13 +21,13 @@ let print_and_delete ppf file =
   s
 
 let test_file file =
-  let stdout = str "%s_stdout" file in
   let stderr = str "%s_stderr" file in
-  let command = str "gospel check %s > %s 2> %s" file stdout stderr in
+  let command = str "gospel check %s > %s 2> %s" file Filename.null stderr in
   let status = Sys.command command in
-  let output = if status = 0 then stdout else stderr in
-  pr "%a@\n(* @[{gospel_expected|@\n[%d] @[%a@]@\n|gospel_expected}@] *)@\n"
-    print file status print_and_delete output
+  pr "%a@\n" print file;
+  if status <> 0 then
+    pr "(* @[{gospel_expected|@\n[%d] @[%a@]@\n|gospel_expected}@] *)@\n" status
+      print_and_delete stderr
 
 let () =
   let file = Sys.argv.(1) in
