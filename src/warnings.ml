00ae719fd5a85c118e17628e6c73f8e438adc083
Nicolas Osborne <nicolas.osborne@tarides.com>
2024-05-13 15:51:46 +0200

    Add a new error for Functor application
    This commit also sorts the error constructors.

--- src/warnings.ml
+++ src/warnings.ml
@@ -2,47 +2,48 @@ open Ppxlib
 open Utils
 
 type kind =
-  | Return_unit_without_modifies of string
-  | Invalid_coercion_type of string
-  | Coercion_cycle of (string * string * string) list
-  | Coercion_already_defined of (string * string * string) list
-  | Not_a_constructor of string
-  | Formula_expected
-  | Term_expected
-  | Duplicated_variable of string
-  | Unbound_variable of string
-  | Pattern_bad_type of string * string
-  | Bad_type of string * string
+  | Ambiguous_pattern
   | Bad_arity of string * int * int
+  | Bad_record_field of string
+  | Bad_type of string * string
   | Bad_type_arity of string * int * int
-  | Name_clash of string
-  | Syntax_error
+  | Circular_open
+  | Coercion_already_defined of (string * string * string) list
+  | Coercion_cycle of (string * string * string) list
+  | Cyclic_type_declaration of string
+  | Duplicated_argument of string
+  | Duplicated_record_field of string
+  | Duplicated_variable of string
+  | Field_application of string
+  | Formula_expected
+  | Free_variables of string list
+  | Function_symbol_expected of string
+  | Functor_application of string
   | Illegal_character of char
   | Illegal_escape of string * string option
-  | Unterminated_comment
-  | Duplicated_argument of string
-  | Unsupported of string
-  | Type_checking_error of string
-  | Module_not_found of string
-  | Free_variables of string list
+  | Invalid_coercion_type of string
   | Invalid_int_literal of string * char option
-  | Symbol_not_found of string list
-  | Bad_record_field of string
-  | Public_type_invariant of string
-  | Circular_open
-  | Field_application of string
-  | Duplicated_record_field of string
-  | Partial_application of string
-  | Unknown_record_field of string
-  | Cyclic_type_declaration of string
-  | Predicate_symbol_expected of string
-  | Function_symbol_expected of string
-  | Pattern_not_exhaustive of string
-  | Pattern_guard_not_exhaustive of string
-  | Pattern_fully_guarded
-  | Pattern_redundant of string
-  | Ambiguous_pattern
+  | Module_not_found of string
+  | Name_clash of string
+  | Not_a_constructor of string
   | Old_in_precond of string
+  | Partial_application of string
+  | Pattern_bad_type of string * string
+  | Pattern_fully_guarded
+  | Pattern_guard_not_exhaustive of string
+  | Pattern_not_exhaustive of string
+  | Pattern_redundant of string
+  | Predicate_symbol_expected of string
+  | Public_type_invariant of string
+  | Return_unit_without_modifies of string
+  | Symbol_not_found of string list
+  | Syntax_error
+  | Term_expected
+  | Type_checking_error of string
+  | Unbound_variable of string
+  | Unknown_record_field of string
+  | Unsupported of string
+  | Unterminated_comment
 
 type error = location * kind
 
@@ -57,103 +58,104 @@ open Fmt
 let function_ ppf (f, t1, t2) = pf ppf "%s: %s -> %s" f t1 t2
 
 let pp_kind ppf = function
-  | Return_unit_without_modifies f ->
+  | Ambiguous_pattern ->
       pf ppf
-        "The function %s returns unit@ but its specifications does not contain \
-         any modifies clause"
-        f
-  | Invalid_coercion_type f ->
-      pf ppf "The function %s does not have a valid coercion type" f
-  | Coercion_cycle fl ->
-      pf ppf "This coercion introduces a cycle:@\n  @[%a@]"
-        (list ~sep:newline function_)
-        fl
-  | Coercion_already_defined fl ->
-      pf ppf "A coercion between these types already exists:@\n  @[%a@]"
-        (list ~sep:newline function_)
-        fl
-  | Not_a_constructor f -> pf ppf "The symbol %s is not a constructor" f
-  | Formula_expected -> pf ppf "A formula was expected"
-  | Term_expected -> pf ppf "A term was expected"
-  | Duplicated_variable s ->
-      pf ppf "The variable %s is duplicated in this pattern" s
-  | Unbound_variable s ->
-      pf ppf "The variable %s does not appear in this pattern" s
-  | Pattern_bad_type (t1, t2) ->
-      pf ppf
-        "This pattern matches values of type %s@ but a pattern was expected \
-         which matches values of type@ %s"
-        t1 t2
-  | Bad_type (t1, t2) ->
-      pf ppf "This term has type %s@ but a term was expected of type@ %s" t1 t2
+        "Or-patterns are prohibited under guards to avoid ambiguities@ (see \
+         OCaml compiler warning 57)"
   | Bad_arity (f, expected, got) ->
       pf ppf
         "The constructor %s expects %d argument(s)@ but is applied to %d \
          argument(s) here"
         f expected got
-  | Name_clash s ->
-      pf ppf "A declaration for %s already exists in this context" s
-  | Syntax_error -> pf ppf "Syntax error"
+  | Bad_record_field f -> pf ppf "The record field %s does not exist" f
+  | Bad_type (t1, t2) ->
+      pf ppf "This term has type %s@ but a term was expected of type@ %s" t1 t2
+  | Bad_type_arity (t, expected, got) ->
+      pf ppf
+        "The type %s expects %d argument(s)@ but was given %d argument(s) here"
+        t expected got
+  | Circular_open -> pf ppf "This open introduces a dependency cycle"
+  | Coercion_already_defined fl ->
+      pf ppf "A coercion between these types already exists:@\n  @[%a@]"
+        (list ~sep:newline function_)
+        fl
+  | Coercion_cycle fl ->
+      pf ppf "This coercion introduces a cycle:@\n  @[%a@]"
+        (list ~sep:newline function_)
+        fl
+  | Cyclic_type_declaration t ->
+      pf ppf "The type declaration for %s contains a cycle" t
+  | Duplicated_argument arg -> pf ppf "Duplicated argument %s" arg
+  | Duplicated_record_field f ->
+      pf ppf "A record field with name %s already exists" f
+  | Duplicated_variable s ->
+      pf ppf "The variable %s is duplicated in this pattern" s
+  | Field_application f -> pf ppf "The record field %s cannot be applied" f
+  | Formula_expected -> pf ppf "A formula was expected"
+  | Free_variables vl ->
+      pf ppf "Unbound variables: %a" (list ~sep:comma string) vl
+  | Function_symbol_expected s -> pf ppf "Not a function symbol: %s" s
+  | Functor_application s -> pf ppf "Functor application not supported: %s" s
   | Illegal_character c -> pf ppf "Illegal character %c" c
-  | Unterminated_comment -> pf ppf "Unterminated comment"
   | Illegal_escape (s, explanation) ->
       pf ppf "Illegal backslash escape in string or character (%s)%a" s
         (option (fmt ": %s"))
         explanation
-  | Duplicated_argument arg -> pf ppf "Duplicated argument %s" arg
-  | Unsupported s -> pf ppf "Not yet supported: %s" s
-  | Type_checking_error msg -> pf ppf "Type checking error: %a" text msg
-  | Module_not_found m -> pf ppf "No module with name %s" m
-  | Free_variables vl ->
-      pf ppf "Unbound variables: %a" (list ~sep:comma string) vl
+  | Invalid_coercion_type f ->
+      pf ppf "The function %s does not have a valid coercion type" f
   | Invalid_int_literal (s, c) ->
       pf ppf "Invalid int literal: %s%a" s (option char) c
+  | Module_not_found m -> pf ppf "No module with name %s" m
+  | Name_clash s ->
+      pf ppf "A declaration for %s already exists in this context" s
+  | Not_a_constructor f -> pf ppf "The symbol %s is not a constructor" f
+  | Old_in_precond precond ->
+      pf ppf "old operator is not allowed in %s clauses" precond
+  | Partial_application s ->
+      pf ppf "The symbol %s cannot be partially applied" s
+  | Pattern_bad_type (t1, t2) ->
+      pf ppf
+        "This pattern matches values of type %s@ but a pattern was expected \
+         which matches values of type@ %s"
+        t1 t2
+  | Pattern_fully_guarded ->
+      pf ppf "All clauses in this pattern-matching are guarded"
+  | Pattern_guard_not_exhaustive p ->
+      pf ppf
+        "This pattern-matching may not be exhaustive because of the guard.@\n\
+         Here is an example of a case that may not be matched:@\n\
+        \  %s" p
+  | Pattern_not_exhaustive p ->
+      pf ppf
+        "This pattern-matching is not exhaustive.@\n\
+         Here is an example of a case that is not matched:@\n\
+        \  %s" p
+  | Pattern_redundant p ->
+      pf ppf
+        "The pattern-matching is redundant.@\n\
+         Here is a case that is unused:@\n\
+        \  %s" p
+  | Predicate_symbol_expected s -> pf ppf "Not a predicate symbol: %s" s
+  | Public_type_invariant t -> pf ppf "Invariant on public type %s" t
+  | Return_unit_without_modifies f ->
+      pf ppf
+        "The function %s returns unit@ but its specifications does not contain \
+         any modifies clause"
+        f
   | Symbol_not_found sl ->
       pf ppf
         "Symbol %a not found in scope@ (see \"Symbols in scope\" documentation \
          page)"
         (list ~sep:(const string ".") string)
         sl
-  | Bad_record_field f -> pf ppf "The record field %s does not exist" f
-  | Public_type_invariant t -> pf ppf "Invariant on public type %s" t
-  | Circular_open -> pf ppf "This open introduces a dependency cycle"
-  | Field_application f -> pf ppf "The record field %s cannot be applied" f
-  | Duplicated_record_field f ->
-      pf ppf "A record field with name %s already exists" f
-  | Partial_application s ->
-      pf ppf "The symbol %s cannot be partially applied" s
+  | Syntax_error -> pf ppf "Syntax error"
+  | Term_expected -> pf ppf "A term was expected"
+  | Type_checking_error msg -> pf ppf "Type checking error: %a" text msg
+  | Unbound_variable s ->
+      pf ppf "The variable %s does not appear in this pattern" s
   | Unknown_record_field s -> pf ppf "The field %s is unknown" s
-  | Bad_type_arity (t, expected, got) ->
-      pf ppf
-        "The type %s expects %d argument(s)@ but was given %d argument(s) here"
-        t expected got
-  | Cyclic_type_declaration t ->
-      pf ppf "The type declaration for %s contains a cycle" t
-  | Predicate_symbol_expected s -> pf ppf "Not a predicate symbol: %s" s
-  | Function_symbol_expected s -> pf ppf "Not a function symbol: %s" s
-  | Pattern_not_exhaustive p ->
-      pf ppf
-        "This pattern-matching is not exhaustive.@\n\
-         Here is an example of a case that is not matched:@\n\
-        \  %s" p
-  | Pattern_guard_not_exhaustive p ->
-      pf ppf
-        "This pattern-matching may not be exhaustive because of the guard.@\n\
-         Here is an example of a case that may not be matched:@\n\
-        \  %s" p
-  | Ambiguous_pattern ->
-      pf ppf
-        "Or-patterns are prohibited under guards to avoid ambiguities@ (see \
-         OCaml compiler warning 57)"
-  | Pattern_fully_guarded ->
-      pf ppf "All clauses in this pattern-matching are guarded"
-  | Pattern_redundant p ->
-      pf ppf
-        "The pattern-matching is redundant.@\n\
-         Here is a case that is unused:@\n\
-        \  %s" p
-  | Old_in_precond precond ->
-      pf ppf "old operator is not allowed in %s clauses" precond
+  | Unsupported s -> pf ppf "Not yet supported: %s" s
+  | Unterminated_comment -> pf ppf "Unterminated comment"
 
 let styled_list l pp = List.fold_left (fun acc x -> styled x acc) pp l
 
