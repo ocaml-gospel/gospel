d93fd0103fe80b6c88d47c71008e1443a867ebc9
Nicolas Osborne <nicolas.osborne@tarides.com>
2024-07-18 18:34:16 +0200

    Add inlined record expected error
    Name and message are shamelessly borrowed from OCaml compiler

--- src/warnings.ml
+++ src/warnings.ml
@@ -21,6 +21,7 @@ type kind =
   | Functor_application of string
   | Illegal_character of char
   | Illegal_escape of string * string option
+  | Inlined_record_expected
   | Invalid_coercion_type of string
   | Invalid_int_literal of string * char option
   | Label_missing of string list
@@ -102,6 +103,8 @@ let pp_kind ppf = function
       pf ppf "Illegal backslash escape in string or character (%s)%a" s
         (option (fmt ": %s"))
         explanation
+  | Inlined_record_expected ->
+      pf ppf "This constructor expects an inlined record argument"
   | Invalid_coercion_type f ->
       pf ppf "The function %s does not have a valid coercion type" f
   | Invalid_int_literal (s, c) ->
