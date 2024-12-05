d8a1d631983d90c3f81ed9fe42aa123504fa457e
Nicolas Osborne <nicolas.osborne@tarides.com>
2024-12-05 15:48:49 +0100

    Fix inlined record pattern

--- test/issues/pattern_inlined_record.mli
+++ test/issues/pattern_inlined_record.mli
@@ -4,9 +4,3 @@
       match a with
        | A { a } -> a
 *)
-(* {gospel_expected|
-   [125] File "pattern_inlined_record.mli", line 5, characters 11-16:
-         5 |        | A { a } -> a
-                        ^^^^^
-         Error: Unbound record field: a.
-   |gospel_expected} *)
