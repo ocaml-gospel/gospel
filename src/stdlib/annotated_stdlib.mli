03debca4d61588a6de2a442db77d14dbb7304886
Nicolas Osborne <nicolas.osborne@tarides.com>
2024-12-20 15:26:13 +0100

    Add default model to list

--- src/stdlib/annotated_stdlib.mli
+++ src/stdlib/annotated_stdlib.mli
@@ -3,3 +3,6 @@ type annotated_int = int
 
 type 'a annotated_array = 'a array
 (*@ mutable model array_content : 'a sequence *)
+
+type 'a annotated_list = 'a list
+(*@ mutable model list_content : 'a sequence *)
