761e6ae1ea2f81da445087ca225cbd75c75800d0
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-01-16 18:42:33 +0100

    Check for repeated name of modules and module types

--- test/typechecker/multiple_module_definition.mli
+++ test/typechecker/multiple_module_definition.mli
@@ -1,2 +1,9 @@
 module M : sig end
 module M : sig end
+(* {gospel_expected|
+   [125] File "multiple_module_definition.mli", line 2, characters 0-18:
+         2 | module M : sig end
+             ^^^^^^^^^^^^^^^^^^
+         Error: Multiple definition of the module name M.
+                Names must be unique in a given signature.
+   |gospel_expected} *)
--- test/typechecker/multiple_module_type.mli
+++ test/typechecker/multiple_module_type.mli
@@ -1,2 +1,9 @@
 module type S = sig end
 module type S = sig end
+(* {gospel_expected|
+   [125] File "multiple_module_type.mli", line 2, characters 0-23:
+         2 | module type S = sig end
+             ^^^^^^^^^^^^^^^^^^^^^^^
+         Error: Multiple definition of the module type name S.
+                Names must be unique in a given signature.
+   |gospel_expected} *)
