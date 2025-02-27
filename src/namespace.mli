18583c73d3f80fb403124538a329fdb75eafdc52
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-02-27 12:22:03 +0100

    Create module to track top level definitions

---
+++ src/namespace.mli
@@ -0,0 +1,49 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  GOSPEL -- A Specification Language for OCaml                          *)
+(*                                                                        *)
+(*  Copyright (c) 2018- The VOCaL Project                                 *)
+(*                                                                        *)
+(*  This software is free software, distributed under the MIT license     *)
+(*  (as described in file LICENSE enclosed).                              *)
+(**************************************************************************)
+
+(** This module provides operations to keep track of all names defined in the
+    top level. *)
+
+type env
+(** The (immutable) environment that keeps track of the definitions in the top
+    level. *)
+
+type mod_defs
+(** A set of top level definitions *)
+
+val scope : env -> mod_defs
+(** Gets the set of definitions in scope *)
+
+val defs : env -> mod_defs
+(** Gets the set of definitions contained within the submodule we are currently
+    processing. *)
+
+(* Records containing the information necessary regarding each top level
+    definitions *)
+type fun_info = { fid : Ident.t }
+type ty_info = { tid : Ident.t; tarity : int }
+type mod_info = { mid : Ident.t; mdefs : mod_defs }
+
+(* Functions to update the environment by adding a top level definition *)
+val add_fun : env -> Ident.t -> env
+val add_type : env -> Ident.t -> int -> env
+val add_mod : env -> Ident.t -> mod_defs -> env
+
+(* These functions receive a qualified identifier and return a fully resolved
+   identifier and the information associated with it. *)
+val type_qualid : mod_defs -> Parse_uast.qualid -> Id_uast.qualid * ty_info
+val fun_qualid : mod_defs -> Parse_uast.qualid -> Id_uast.qualid * fun_info
+
+val empty_env : env
+(** The empty environment. The only names in scope are Gospel primitive types *)
+
+val submodule : env -> env
+(** Returns a new environment for processing a submodule. The variables in scope
+    remain the same and the set of definitions is empty. *)
--- src/typing.ml
+++ src/typing.ml
@@ -8,95 +8,10 @@
 (*  (as described in file LICENSE enclosed).                              *)
 (**************************************************************************)
 
-open Id_uast
 module Env = Map.Make (String)
+open Id_uast
 module W = Warnings
-
-type fun_info = { fid : Ident.t }
-type ty_info = { tid : Ident.t; tarity : int }
-
-type mod_info = { mid : Ident.t; mdefs : mod_defs }
-
-and mod_defs = {
-  fun_env : fun_info Env.t; (* Function definitions *)
-  type_env : ty_info Env.t; (* Type definitions *)
-  mod_env : mod_info Env.t; (* Nested modules *)
-}
-(** Set of top level module definitions *)
-
-(** Represents all the definitions within a module *)
-let empty_defs =
-  { fun_env = Env.empty; type_env = Env.empty; mod_env = Env.empty }
-
-(** [lookup f defs pid] accesses the namespace [f defs] and returns the data
-    associated with [pid].
-    @raise Not_found
-      when there is no identifer with name [pid.pid_str] in the namespace
-      associated with [f defs] *)
-let lookup f (defs : mod_defs) pid =
-  let env = f defs in
-  let str = pid.Preid.pid_str in
-  Env.find str env
-
-(* Helper functions for field accesses *)
-
-let find_fun = fun d -> d.fun_env
-let find_type = fun d -> d.type_env
-let find_mod = fun d -> d.mod_env
-
-(* Helper functions to add top level definitions into the environment. *)
-
-let add_fun fid defs =
-  let env = defs.fun_env in
-  let info = { fid } in
-  { defs with fun_env = Env.add fid.Ident.id_str info env }
-
-let add_mod mid mdefs defs =
-  let menv = defs.mod_env in
-  let info = { mid; mdefs } in
-  { defs with mod_env = Env.add mid.Ident.id_str info menv }
-
-let add_type tid tarity defs =
-  let tenv = defs.type_env in
-  let info = { tid; tarity } in
-  { defs with type_env = Env.add tid.Ident.id_str info tenv }
-
-(** [access_mod defs q] returns a qualified identifer where the identifiers used
-    in [q] have been replaced with uniquely tagged identifiers. This function
-    assumes that all the identifiers in [q] are belong to the module namespace.
-    This function also returns the definitions contained in the module
-    associated with [q].
-    @raise Not_found
-      if any of the identifiers in [q] are not valid module identifiers. TODO,
-      This exception should be caught here and subsequently throw a Gospel
-      exception. *)
-let rec access_mod defs = function
-  | Parse_uast.Qid pid ->
-      let info = lookup find_mod defs pid in
-      let id = info.mid in
-      (Qid id, info.mdefs)
-  | Qdot (q, pid) ->
-      let q, defs = access_mod defs q in
-      let info = lookup find_mod defs pid in
-      let id = info.mid in
-      let defs = info.mdefs in
-      (Qdot (q, id), defs)
-
-(** [unique_toplevel_qualid f defs q] returns the information associated with
-    the name [q]. Additionally, if [q] is of the type [M1.M2...Mn.id], returns
-    an optional whose value is the prefix [M1.M2...Mn], where every module
-    access has been resolved. Otherwise, returns [None] *)
-let unique_toplevel_qualid f defs = function
-  | Parse_uast.Qid pid ->
-      (* If there are no module accesses, we lookup [id] in the
-       environment [f defs]. *)
-      (None, lookup f defs pid)
-  | Qdot (q, pid) ->
-      (* If there are module accesses, we first get the environment
-       associated with the module represented by [q], and then lookup
-       [id] in that environment. *)
-      let q, defs = access_mod defs q in
-      (Some q, lookup f defs pid)
+open Namespace
 
 type local_env = {
   term_var : Ident.t Env.t;
@@ -118,8 +33,6 @@ let add_term_var var id env =
 let add_type_var var id env =
   { env with type_var = Env.add var id env.type_var }
 
-let mk_qid q id = match q with None -> Qid id | Some q -> Qdot (q, id)
-
 (** [unique_pty defs env pty] Returns a type annotation where each type symbol
     (including type variables) has been replaced with a unique identifier. Type
     variables are searched in [env] and type names are search in [defs]. Type
@@ -135,9 +48,8 @@ let unique_pty defs env =
         env := add_type_var pid.pid_str id !env;
         PTtyvar id
     | PTtyapp (q, l) ->
-        let q, tinfo = unique_toplevel_qualid find_type defs q in
+        let q, tinfo = type_qualid defs q in
         let id = tinfo.tid in
-        let q = mk_qid q id in
         let expected = tinfo.tarity in
         let arity = List.length l in
         if arity <> expected then
@@ -162,8 +74,7 @@ let unique_var env defs q =
       Id_uast.Qid (Env.find pid.pid_str env)
   | _ ->
       (* If [q] is of the form [Qdot] it cannot be a local variable *)
-      let q, info = unique_toplevel_qualid find_fun defs q in
-      mk_qid q info.fid
+      fst (fun_qualid defs q)
 
 (** [unique_term defs env t] returns a term where every variable in [t] has been
     replaced with a uniquely tagged variable. When we find a free variable, we
@@ -281,29 +192,12 @@ let ghost_type_decl t =
     tloc = t.tloc;
   }
 
-type env = {
-  defs : mod_defs;
-  (* Contains the top level definitions in the current module *)
-  scope : mod_defs;
-      (* Contains the top level definitions currently in scope. A definition is in
-     scope if it has been previously defined or exposed through an [open]. Not
-     necessarily a subset of [defs_mod], since opening modules can shadow
-     previous definitions. *)
-}
-(** Environment for processing top level signatures. *)
-
-(** [add_def f env] applies the function [f] to the list of module definitions
-    and to the scope of the environment. This is useful when adding new
-    definitions to a module, since these have to also be added to the current
-    scope. *)
-let add_def f env = { defs = f env.defs; scope = f env.scope }
-
 let rec process_module env m =
   (* TODO figure out when this is None *)
   let id = Option.map Ident.from_preid m.Parse_uast.mdname in
   (* Since we are now at the beginning of a new module, we create an environment
      with the same variables in scope, but with no module definitions. *)
-  let sub_mod_env = { env with defs = empty_defs } in
+  let sub_mod_env = submodule env in
   let mdesc, mod_defs =
     match m.mdtype.mdesc with
     | Mod_signature m ->
@@ -312,16 +206,15 @@ let rec process_module env m =
         let s, env = signatures m sub_mod_env in
         (* We ignore the [scope] field in [env] as it is not relevant after
            processing the module. *)
-        (Mod_signature s, env.defs)
+        (Mod_signature s, defs env)
     | _ -> assert false
   in
   (* If this module has an identifier, then this function adds it to the current
      definitions *)
-  let f defs =
-    Option.fold ~none:defs ~some:(fun id -> add_mod id mod_defs defs) id
+  let env =
+    Option.fold ~none:env ~some:(fun id -> add_mod env id mod_defs) id
   in
   (* Rebuild the module object *)
-  let env = add_def f env in
   let mloc = m.mdtype.mloc in
   let mattributes = m.mdtype.mattributes in
   let mdtype = { mdesc; mloc; mattributes } in
@@ -346,20 +239,20 @@ and signature s env =
 
 and gospel_sig env = function
   | Parse_uast.Sig_function f ->
-      let f = function_ f env.scope in
+      let f = function_ f (scope env) in
       (* Adds [f] to the set of variables defined and to the
         scope *)
-      let env = add_def (add_fun f.fun_name) env in
+      let env = add_fun env f.fun_name in
       (Sig_function f, env)
   | Sig_axiom ax ->
       (* Since axioms cannot be referenced, the environment is not
         modified.*)
-      let ax = axiom env.scope ax in
+      let ax = axiom (scope env) ax in
       (Sig_axiom ax, env)
   | Sig_ghost_type t ->
       let t = ghost_type_decl t in
       let arity = List.length t.tparams in
-      let env = add_def (add_type t.tname arity) env in
+      let env = add_type env t.tname arity in
       (Sig_ghost_type t, env)
   | _ -> assert false
 
@@ -374,12 +267,5 @@ and signatures (l : Parse_uast.s_signature) env =
       (s :: t, env)
 
 (** An environment with primitive type definitions. *)
-let type_env =
-  List.fold_left
-    (fun tenv (x, y) -> Env.add x { tid = y; tarity = 0 } tenv)
-    Env.empty Structure.primitive_list
-
-(** The empty environment *)
-let empty_env = { defs = empty_defs; scope = { empty_defs with type_env } }
 
 let signatures l = fst (signatures l empty_env)
