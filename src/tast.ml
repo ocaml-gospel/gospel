a38b59c5d8d4be840fab7723d423df0052d32cf7
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-03-04 18:50:07 +0100

    Process attributes

--- src/tast.ml
+++ src/tast.ml
@@ -81,6 +81,7 @@ and s_signature_item_desc =
   | Sig_axiom of axiom
   | Sig_module of s_module_declaration
   | Sig_ghost_type of Id_uast.s_type_declaration
+  | Sig_attribute of Ppxlib.attribute
 
 and s_signature_item = { sdesc : s_signature_item_desc; sloc : Location.t }
 and s_signature = s_signature_item list
