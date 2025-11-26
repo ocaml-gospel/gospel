1db21bebafd97517e4606b387718fab0315d1026
Nicolas Osborne <nicolas.osborne@tarides.com>
2025-11-26 16:46:21 +0100

    Make type checker ignore unsupported constructs
    This commit makes Gospel type checker stop failling when it encounters
    an unsupported construct.
    
    It also silently ignore the Gospel annotations if any, which is
    questionable behaviour.

--- test/typechecking/unsupported/polymorphic_variant.mli
+++ test/typechecking/unsupported/polymorphic_variant.mli
@@ -1,7 +1,1 @@
 type t = [ `A | `B ]
-(* {gospel_expected|
-[125] gospel: internal error, uncaught exception:
-              File "src/uattr2spec.ml", line 85, characters 9-15: Assertion failed
-              
-      
-|gospel_expected} *)
--- test/typechecking/unsupported/type_extension_with_spec.mli
+++ test/typechecking/unsupported/type_extension_with_spec.mli
@@ -1,8 +1,2 @@
 type t = ..
 (*@ model : integer *)
-(* {gospel_expected|
-[125] gospel: internal error, uncaught exception:
-              File "src/uattr2spec.ml", line 102, characters 9-15: Assertion failed
-              
-      
-|gospel_expected} *)
