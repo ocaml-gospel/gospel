4617c4538120e15187a70fdd120d7e5250ac32b2
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-12-09 14:09:21 +0000

    Tests for duplicate names in ownership clauses

---
+++ test/duplicates/duplicate_consumes_top_level3.mli
@@ -0,0 +1,24 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  GOSPEL -- A Specification Language for OCaml                          *)
+(*                                                                        *)
+(*  Copyright (c) 2018- The VOCaL Project                                 *)
+(*                                                                        *)
+(*  This software is free software, distributed under the MIT license     *)
+(*  (as described in file LICENSE enclosed).                              *)
+(**************************************************************************)
+
+val x : int ref
+val y : int ref
+val f : unit -> unit
+(*@ consumes x
+    consumes x
+    consumes y *)
+
+(* {gospel_expected|
+[1] File "duplicate_consumes_top_level3.mli", line 11, characters 4-5:
+    11 | val x : int ref
+             ^
+    Error: The variable x is listed as consumes twice
+    
+|gospel_expected} *)
---
+++ test/duplicates/duplicate_modifies_top_level1.mli
@@ -0,0 +1,22 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  GOSPEL -- A Specification Language for OCaml                          *)
+(*                                                                        *)
+(*  Copyright (c) 2018- The VOCaL Project                                 *)
+(*                                                                        *)
+(*  This software is free software, distributed under the MIT license     *)
+(*  (as described in file LICENSE enclosed).                              *)
+(**************************************************************************)
+
+val x : int ref
+val f : unit -> unit
+(*@ modifies x
+    modifies x *)
+
+(* {gospel_expected|
+[1] File "duplicate_modifies_top_level1.mli", line 11, characters 4-5:
+    11 | val x : int ref
+             ^
+    Error: The variable x is listed as modified twice
+    
+|gospel_expected} *)
