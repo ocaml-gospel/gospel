56e0392083fc2b43d5e89822aa870b0390d10909
Nicolas Osborne <nicolas.osborne@tarides.com>
2026-06-04 10:39:53 +0200

    Add some more test about unsupported constructs

---
+++ test/typechecking/unsupported/variant_as_argument.mli
@@ -0,0 +1,9 @@
+type t = A | B
+
+val f : t -> bool
+(* {gospel_expected|
+[125] gospel: internal error, uncaught exception:
+              File "src/uattr2spec.ml", line 102, characters 9-15: Assertion failed
+              
+      
+|gospel_expected} *)
